181c2651bec960433966b989ed4fb4816a2e922d
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2017-07-06 19:10:00 +0100

    Adding vector versions of sub benchmark

--- C/mpiio_sub_read_vector.c
+++ C/mpiio_sub_read_vector.c
@@ -26,19 +26,21 @@ int main(int argc, char **argv){
 
   int nx, ny, nxp, nyp, xprocs, yprocs, subnum, ioworker, lowerrank, upperrank, barrier;
 
-  MPI_Status status;
+  int tag;
+
   MPI_File fh;
+  MPI_Status status;
   MPI_Offset disp;
 
-  int count, blocklength, stride, floatsize, tag;
+  int count, blocklength, stride, floatsize;
 
   MPI_Datatype my_mpi_vector;
 
   int istart, jstart;
 
-  double starttime, endtime, totaltime;
-
   char filename[MAXFILENAME];
+
+  double starttime, endtime, totaltime;
 
   MPI_Comm comm = MPI_COMM_WORLD;
   MPI_Comm iocomm;
@@ -52,13 +54,13 @@ int main(int argc, char **argv){
 
   pcoords = (int **)arralloc(sizeof(int), 2, size, NDIM);
   x = (float **)arralloc(sizeof(float),2,nxp,nyp);
+
   /*
    *  Work out the coordinates of all the processes in the grid and
    *  print them out
    */
 
   initpgrid(&pcoords[0][0], xprocs, yprocs);
-
 
   if (rank == 0)
     {
@@ -77,12 +79,12 @@ int main(int argc, char **argv){
   /*
    *  Initialise the arrays to a grey value
    */
+
   initarray(&x[0][0]  , nxp, nyp);
 
   if(barrier){
     MPI_Barrier(comm);
   }
-
 
   starttime = MPI_Wtime();
 
@@ -102,99 +104,103 @@ int main(int argc, char **argv){
 
   MPI_Type_commit(&my_mpi_vector);
 
+
   /* Because file open is a collective operation, create a communicator for ioworkers and for non-ioworkers.*/
   /* This can then be used for a subset of all processes to do the I/O.*/
   MPI_Comm_split(comm, ioworker, rank, &iocomm);
 
-  /*
-   *  Open the file for reading only and attach to file handle fh
-   *  No IO hints are passed since MPI_INFO_NULL is specified
-   */
-
-  if (MPI_File_open(iocomm, argv[1], MPI_MODE_RDONLY,
-		    MPI_INFO_NULL, &fh) != MPI_SUCCESS)
-    {
-      printf("Open error on rank %d\n", rank);
-    }
-
-  /*
-   *  Set view for this process using the vector type with an appropriate
-   *  value if disp (computed in BYTES!)
-   */
-
-  MPI_Type_size(MPI_FLOAT, &floatsize);
   tag = 11;
 
-
   if(ioworker == 1){
-    // Read other ranks data and send it.
-    for(i=lowerrank+1;i<=upperrank;i++){
-      istart = pcoords[i][0]*nxp;
-      jstart = pcoords[i][1]*nyp;     
     
+    /*
+     *  Open the file for reading only and attach to file handle fh
+     *  No IO hints are passed since MPI_INFO_NULL is specified
+     */
+    
+    if (MPI_File_open(iocomm, argv[1], MPI_MODE_RDONLY,
+		      MPI_INFO_NULL, &fh) != MPI_SUCCESS)
+      {
+	printf("Open error on rank %d\n", rank);
+      }
+
+    MPI_Type_size(MPI_FLOAT, &floatsize);
+
+    for(i=lowerrank+1;i<=upperrank;i++){
+
+      istart = pcoords[i][0] * nxp;
+      jstart = pcoords[i][1] * nyp;
+      
       disp = istart;
       disp = disp*(MPI_Offset)ny;
       disp = disp + (MPI_Offset)jstart;
       disp = disp*(MPI_Offset)floatsize;
-      
+
       /*
        *  Set view for this process using appropriate datatype
        */
+      
       if (MPI_File_set_view(fh, disp, MPI_FLOAT, my_mpi_vector, "native",
-			    MPI_INFO_NULL) != MPI_SUCCESS){
-	printf("View error on rank %d\n", rank);
-      }
+			  MPI_INFO_NULL) != MPI_SUCCESS)
+	{
+	  printf("View error on rank %d\n", rank);
+	}
       
       /*
        *  Read all the data for this process (ie nxp*nyp floats)
        */
-      
-      if (MPI_File_read(fh, &x[0][0], nxp*nyp, MPI_FLOAT, &status) != MPI_SUCCESS){
-	printf("Read error on rank %d\n", rank);
-      }
+
+      if (MPI_File_read(fh, &x[0][0], nxp*nyp, MPI_FLOAT, &status) != MPI_SUCCESS)
+	{
+	  printf("Read error on rank %d\n", rank);
+	}
+
+
       MPI_Ssend(&x[0][0], nxp*nyp, MPI_FLOAT, i, tag, comm);
     }
-    
-    // Read this rank's data
-    istart = pcoords[rank][0]*nxp;
-    jstart = pcoords[rank][1]*nyp;
-    
-    
+
+    istart = pcoords[rank][0] * nxp;
+    jstart = pcoords[rank][1] * nyp;
+
     disp = istart;
     disp = disp*(MPI_Offset)ny;
     disp = disp + (MPI_Offset)jstart;
     disp = disp*(MPI_Offset)floatsize;
-    
+            
+
     /*
      *  Set view for this process using appropriate datatype
      */
+    
     if (MPI_File_set_view(fh, disp, MPI_FLOAT, my_mpi_vector, "native",
-			  MPI_INFO_NULL) != MPI_SUCCESS){
-      printf("View error on rank %d\n", rank);
-    }
+			  MPI_INFO_NULL) != MPI_SUCCESS)
+      {
+	printf("View error on rank %d\n", rank);
+      }
     
-    /*
-     *  Read all the data for this process (ie nxp*nyp floats)
-     */
+      /*
+       *  Read all the data for this process (ie nxp*nyp floats)
+       */
     
-    if (MPI_File_read(fh, &x[0][0], nxp*nyp, MPI_FLOAT, &status) != MPI_SUCCESS){
-      printf("Read error on rank %d\n", rank);
-    }
+    if (MPI_File_read(fh, &x[0][0], nxp*nyp, MPI_FLOAT, &status) != MPI_SUCCESS)
+      {
+	printf("Read error on rank %d\n", rank);
+	}
     
-
-  }else{
-    // If I'm not an I/O worker receive our data
-    MPI_Recv(&x[0][0], nxp*nyp, MPI_FLOAT, lowerrank, tag, comm, &status);
-  }
     
     /*
      *  Close file
      */
-
-  if (MPI_File_close(&fh) != MPI_SUCCESS)
-    {
-      printf("Close error on rank %d\n", rank);
+    
+    if (MPI_File_close(&fh) != MPI_SUCCESS){
+	printf("Close error on rank %d\n", rank);
     }
+
+  }else{
+    // If I'm not an I/O worker receive our data
+    MPI_Recv(&x[0][0], nxp*nyp, MPI_FLOAT, lowerrank, tag, comm, &status);    
+  }
+
 
   endtime = MPI_Wtime();
 
@@ -202,6 +208,7 @@ int main(int argc, char **argv){
    *  Every process writes out its local data array x to an individually
    *  named file which has the rank appended to the file name
    */
+
 #ifdef DEBUG
   createfilename(filename, "coutput", nxp, nyp, rank);
   iowrite(filename, &x[0][0], nxp*nyp);
@@ -211,6 +218,7 @@ int main(int argc, char **argv){
 
   dotimings(totaltime, rank, size);
 
+  MPI_Type_free(&my_mpi_vector);
   MPI_Comm_free(&iocomm);
   MPI_Finalize();
   free(x);
